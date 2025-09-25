db68ef7cc3d227317de0cc02c6bd48b50f26d52e
Steven C. <steventcruise@gmail.com>
2025-09-24 20:53:26 -0400

    Added Pictures and readme

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Chat Indicators+
+
+Adds a text tag to chat messages to easily identify whether the message is a whisper, blind roll, GM roll, or self roll.
+
+There is also an option to automatically reveal Player rolled heals or GM Damage rolls
+
+## Confirmed System Compatability
+*PF2e
+
+Inspired by Chat "Message Accessibility Indicators" which was archived by the owner on Dec 3, 2021
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,43 +1,30 @@
 Hooks.on("i18nInit", () => {
 
-// game.settings.register("chat-indicators-plus", "tagPublicRolls", {
-//     name: "Display indicators on public roll?",
-//     hint: "Do you want [Public Roll] displayed?",
-//     scope: "world",     // This specifies a client-stored setting
-//     config: true,        // This specifies that the setting appears in the configuration view
-//     requiresReload: true, // This will prompt the user to reload the application for the setting to take effect.
-//     type: String,
-//     choices: {           // If choices are defined, the resulting setting will be a select menu
-//         0: "Hidden",
-//         1: "Shown"
-//       },
-//     default: 1       // The default value for the setting
-//   });
-
-game.settings.register("chat-indicators-plus", "autoRevealGMDamage", {
+  game.settings.register("chat-indicators-plus", "autoRevealGMDamage", {
     name: "Overide GM Damage rolls to be public",
     hint: "Do you want GM Damage rolls always made shown as public?",
-    scope: "world",     // This specifies a client-stored setting
-    config: true,        // This specifies that the setting appears in the configuration view
-    requiresReload: true, // This will prompt the user to reload the application for the setting to take effect.
+    scope: "world",     
+    config: true,        
+    requiresReload: true, 
     type: String,
-    choices: {           // If choices are defined, the resulting setting will be a select menu
+    choices: {           
         0: "Use Default",
         1: "Public GM Damage rolls"
       },
-    default: 0       // The default value for the setting
+    default: 0
   })
+  
 game.settings.register("chat-indicators-plus", "autoRevealPlayerHeal", {
     name: "Overide player heals to be public",
     hint: "Do you want the player rolled heals to be shown as public?",
-    scope: "world",     // This specifies a client-stored setting
-    config: true,        // This specifies that the setting appears in the configuration view
-    requiresReload: true, // This will prompt the user to reload the application for the setting to take effect.
+    scope: "world",  
+    config: true,        
+    requiresReload: true, 
     type: String,
-    choices: {           // If choices are defined, the resulting setting will be a select menu
+    choices: {
         0: "Use Default",
         1: "Public player heals"
       },
-    default: 0       // The default value for the setting
+    default: 0
   })
 });
